f89d682c88badde8eb3e3435a7e178d4aea04de0
Firas Abuzaid <fabuzaid21@gmail.com>
2013-09-13 14:15:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NytimesHeadlines::Application.config.secret_token = '5aa94e45cdd0c9b191ec0de77b4450c3ea473e0b872707f2d21b8c90f841259094b67f986f8a315719916d8bcdb3e2365a12658b8679c0ce96da5b145597f407'
